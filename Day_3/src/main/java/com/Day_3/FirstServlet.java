9fa2b477daef121586611c1ebc2422522937f2f8
Abhinav Singh <abhinavsingh26@gmail.com>
2022-01-05 20:47:56 +0530

    u

--- Day_3/src/main/java/com/Day_3/FirstServlet.java
+++ Day_3/src/main/java/com/Day_3/FirstServlet.java
@@ -5,50 +5,31 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FirstServlet
- */
 //@WebServlet("/First")
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FirstServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
-		PrintWriter out =  response.getWriter();
-		response.setContentType("text/html"); 
+		PrintWriter out =  res.getWriter();
+		res.setContentType("text/html");
 		
 		out.println("welcome to Http servlet ");
 	}
-
+	
+public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		PrintWriter out =  res.getWriter();
+		res.setContentType("text/html");
+		
+		out.println("welcome to Http servlet ");
+	}
 }
