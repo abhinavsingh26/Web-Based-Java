09a0506d86df86ed499bddda42ae24785cc1c370
Abhinav Singh <abhinavsingh26@gmail.com>
2022-01-07 16:42:14 +0530

    updated

--- Day_3/src/main/java/com/Day_3/FirstServlet.java
+++ Day_3/src/main/java/com/Day_3/FirstServlet.java
@@ -3,25 +3,25 @@ package com.Day_3;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 //@WebServlet("/First")
-public class FirstServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class FirstServlet extends HttpServlet { 
+	
        
 
-public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
 		PrintWriter out =  res.getWriter();
 		res.setContentType("text/html");
 		
-		out.println("welcome to Http servlet ");
+		String name = req.getParameter("name");
+		String password = req.getParameter("password");
+		
+		out.println("welcome"+ name);
+		
 	}
 }
