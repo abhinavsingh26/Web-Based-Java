3fa5de4302faf97ca2e8daf080f5b639b79fdb2f
Abhinav Singh <abhinavsingh26@gmail.com>
2022-01-08 18:08:54 +0530

    new Login Page

--- Day_3/src/main/java/com/Day_3/LoginServlet.java
+++ Day_3/src/main/java/com/Day_3/LoginServlet.java
@@ -1,24 +1,33 @@
 package com.Day_3;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
-@WebServlet("/LoginServlet")
+
+//@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+		PrintWriter out =  response.getWriter();
+		response.setContentType("text/html");
+		
+		if(name.equals("admin") && password.equals("admin")){
+			out.print("Login Successfull...");
+		}else {
+			out.print("Login Failed...");
+		}
+		
 	}
 
 }
