fca09cb7350a416feb576bc150079e44d203b556
Abhinav Singh <abhinavsingh26@gmail.com>
2022-01-15 16:59:02 +0530

    up

--- Emp-Register/src/main/java/com/register/EmployeeDao.java
+++ Emp-Register/src/main/java/com/register/EmployeeDao.java
@@ -1,5 +1,33 @@
 package com.register;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class EmployeeDao {
+public boolean insertEmployee(Employee emp) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login_details","root","eov3zl5ilx7c");
+		
+		System.out.println("Connection got with mysql");
+		
+		String q= "insert into employee values(?,?,?,?) ";
+		PreparedStatement pmt = con.prepareStatement(q);
+		
+		pmt.setString(1, emp.getName());
+		pmt.setString(2, emp.getAddress());
+		pmt.setString(3, emp.getEmail());
+		pmt.setString(4, emp.getContact());
+		
+		int result=pmt.executeUpdate();
+		
+		boolean status;
+		
+		
+	}
 
 }
--- Emp-Register/src/main/java/com/register/EmployeeServlet.java
+++ Emp-Register/src/main/java/com/register/EmployeeServlet.java
@@ -2,6 +2,7 @@ package com.register;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,6 +25,18 @@ public class EmployeeServlet extends HttpServlet {
 		String address = request.getParameter("addresss");
 		String email = request.getParameter("email");
 		String contact = request.getParameter("contact");
+		
+		
+		Employee emp = new Employee(name, address,email, contact);
+		EmployeeDao edao = new EmployeeDao();
+		
+		try {
+			edao.insertEmployee(emp);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
